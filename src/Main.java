b6d97cd161ac126ce8e97f3fce1855405796b93d
cahas <bulatovalexsandr1999@gmail.com>
2023-01-09 21:20:30 +0400

    just 4 task

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1() {
@@ -34,5 +35,24 @@ public class Main {
             System.out.println("Если скорость " + speedCar + " км/ч "
                     + " то  можно ездить спокойно");
         }
+    }
+    public static void task4 () {
+        int age = 19;
+        boolean youShotGtKindergarten = age > 2  || age < 6;
+        if (youShotGtKindergarten) {
+            System.out.println("Если возраст человека равен " + age
+                    + " то ему нужно ходить в детский сад ");}
+        boolean youShotGtSchool = age > 7  || age < 18;
+        if (youShotGtSchool) {
+            System.out.println("Если возраст человека равен " + age
+                    + " то ему нужно ходить в школу ");}
+        boolean youShotGtUniversity = age > 18  || age < 24;
+        if (youShotGtUniversity) {
+            System.out.println("Если возраст человека равен " + age
+                    + " то ему нужно ходить в университет ");}
+        boolean youShotGtWork = age > 24;
+        if (youShotGtWork) {
+            System.out.println("Если возраст человека равен " + age
+                    + " то ему нужно ходить на работу ");}
     }
 }
